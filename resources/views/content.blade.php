cb4a8238178262434e597791c5df890140b963c1
shroukksobhy <shroukksobhy@gmail.com>
2021-02-13 17:23:44 +0200

    intial commint

---
+++ resources/views/content.blade.php
@@ -0,0 +1,154 @@
+<!-- <div class="fakeimg" style="height:200px;"> -->
+<!-- <iframe class="nav-justified fakeimg" style="height:500px" src="https://www.youtube.com/embed/MPFTY-b4WYE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+<!-- 16:9 aspect ratio -->
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/MPFTY-b4WYE"></iframe>
+</div>
+
+<!-- </div> -->
+
+<ul class="nav nav-tabs nav-justified" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#communication">نظرة عامة</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#schedule">ملاحظات</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#messages">الترجمة</a>
+    </li>
+</ul>
+
+<div class="tab-content">
+    <div class="tab-pane fade show active" id="communication">
+        <div class="d-flex flex-column flex-lg-row">
+            <div>
+                <p class="lead text-right">
+                    وريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور
+
+                    أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم فينايم,كيواس نوستريد
+
+                    أكسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواس
+
+                    أيوتي أريري دولار إن ريبريهينديرأيت فوليوبتاتي فيلايت أيسسي كايلليوم دولار أيو فيجايت
+
+                    نيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات نون بروايدينت ,سيونت ان كيولبا
+
+                    كيو أوفيسيا ديسيريونتموليت انيم أيدي ايست لابوريوم </p>
+
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade" id="schedule">
+        <div class="d-flex flex-column flex-lg-row">
+            <div>
+                <p class="lead text-right">
+                    وريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور
+
+                    أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم فينايم,كيواس نوستريد
+
+                    أكسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواس
+
+                    أيوتي أريري دولار إن ريبريهينديرأيت فوليوبتاتي فيلايت أيسسي كايلليوم دولار أيو فيجايت
+
+                    نيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات نون بروايدينت ,سيونت ان كيولبا
+
+                    كيو أوفيسيا ديسيريونتموليت انيم أيدي ايست لابوريوم
+                </p>
+
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade" id="messages">
+        <div class="d-flex flex-column flex-lg-row">
+            <div>
+                <p class="lead text-right">
+                    وريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور
+
+                    أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم فينايم,كيواس نوستريد
+
+                    أكسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواس
+
+                    أيوتي أريري دولار إن ريبريهينديرأيت فوليوبتاتي فيلايت أيسسي كايلليوم دولار أيو فيجايت
+
+                    نيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات نون بروايدينت ,سيونت ان كيولبا
+
+                    كيو أوفيسيا ديسيريونتموليت انيم أيدي ايست لابوريوم
+                </p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+<!-- BOOTSTRAP TAB -->
+<nav>
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-resources" role="tab" aria-controls="nav-home" aria-selected="true">
+            المصادر</a>
+        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-downloads" role="tab" aria-controls="nav-profile" aria-selected="false">
+            التنزيلات</a>
+        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-comments" role="tab" aria-controls="nav-contact" aria-selected="false">
+            <i class="fa fa-comments"></i> التعليقات</a>
+    </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="nav-resources" role="tabpanel" aria-labelledby="nav-home-tab">
+        1
+    </div>
+    <div class="tab-pane fade" id="nav-downloads" role="tabpanel" aria-labelledby="nav-profile-tab">
+
+        <a href="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" download>
+            file to download
+        </a>
+        <a href="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" download>
+            file to download
+        </a>
+        <a href="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" download>
+            file to download
+        </a>
+    </div>
+    <div class="tab-pane fade" id="nav-comments" role="tabpanel" aria-labelledby="nav-contact-tab">
+        <div class="lesson-page text-right">
+            <div class="unRegisterd-comment">يجب <a href="#">تسجيل الدخول</a> أو
+                <a href="#">التسجيل</a> لتتمكّن
+                من التعليق
+            </div>
+            <div class="comments">
+                <div class="comment-box" id="comment-1584">
+                    <div class="row">
+                        <div class="col-md-1 comment-avatar">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <div class="col-md-2">
+                            Shrouk
+                        </div>
+                        <div class="col-md-6">
+                            <p>منذ 9 أشهر</p>
+                        </div>
+                    </div>
+                   
+                    <div class="comment-text">ADADA</div>
+                </div>
+                <div class="comment-box" id="comment-1584">
+                    <div class="row">
+                        <div class="col-md-1 comment-avatar">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <div class="col-md-2">
+                            Shrouk
+                        </div>
+                        <div class="col-md-6">
+                            <p>منذ 9 أشهر</p>
+                        </div>
+                    </div>
+
+                    <div class="comment-text">ADADA</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/courses.blade.php
@@ -0,0 +1,358 @@
+<html dir="rtl" lang="ar">
+
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+  <!-- <script src="assets/jquery-3.2.1.min.js"></script>
+  <link rel="dns-prefetch" href="//fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">-->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <link href="assets/main.css" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/c0074c63b8.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+  <div class="container">
+    <div id="header" class="header">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img src="https://www.coursat.org/images/logo.png" alt="كورسات">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown open">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true"><i class="fa fa-th-large"></i> التصنيفات <span class="caret"></span></a>
+              <ul class="dropdown-menu categories-menu" role="menu">
+                <li>
+                  <a href="#">
+                    لغات </a>
+                </li>
+                <li>
+                  <a href="#">
+                    شبكات الحاسوب </a>
+                </li>
+                <li>
+                  <a href="#">
+                    أمن المعلومات </a>
+                </li>
+                <li>
+                  <a href="#">
+                    قواعد بيانات </a>
+                </li>
+                <li>
+                  <a href="#">
+                    أنظمة تشغيل </a>
+                </li>
+                <li>
+                  <a href="#">
+                    تطوير الويب </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <!-- <a class="nav-link" href="#">الرئيسية <span class="sr-only">(current)</span></a> -->
+              <a href="#" class="sign-up">
+                <i class="fa fa-user-plus"></i>
+                تسجيل
+              </a>
+            </li>
+            <li class="nav-item">
+              <!-- <a class="nav-link" href="#">كورسات</a> -->
+              <a href="{{url('login')}}" class="sign-in"><i class="fa fa-lock"></i> دخول</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <div id="description">
+      <div class="row">
+
+        <div class="col-md-12">
+          <h3>1- المقدمة</h3>
+
+          <ul class="page-meta">
+            <li data-toggle="tooltip" data-placement="top" title="" data-original-title="ترتيب الدرس">
+              <i class="fa fa-sort"></i> ترتيب الدرس : 1
+            </li>
+            <li data-toggle="tooltip" data-placement="top" title="" data-original-title="الكورس">
+              <i class="fa fa-circle-o"></i> <a href="#">احترف برنامج الفوتوشوب Photosop CS6</a>
+            </li>
+            <li data-toggle="tooltip" data-placement="top" title="" data-original-title="مدة الدرس"><i class="fa fa-clock-o"></i> 00:04:22</li>
+            <li data-toggle="tooltip" data-placement="top" title="" data-original-title="المشاهدات"><i class="fa fa-eye"></i> 3,298</li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+          <div class="page-options">
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <!-- <div class="main"> -->
+    <div class="row">
+      <div class="sidebar">
+
+        <div id="accordion" role="tablist" aria-multiselectable="true">
+
+          <div class="card">
+            <div class="card-header" id="headingOne">
+              <h5 class="mb-0 text-center">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  الدرس الاول
+                  <!-- <spin class="circle-icon mr-4">1</spin> -->
+                </button>
+              </h5>
+            </div>
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+
+
+              <div class="container">
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  <a onclick="refresh_links()" href="#">المقدمة</a>
+                </label>
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  المتطلبات
+                </label>
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  الحلول
+                </label>
+              </div>
+              <!-- 
+                <ul class="list-unstyled ui-steps">
+                  <li class="media">
+                    <a onclick="refresh_links()" href="#">حدد مهاراتك</a>
+                  </li>
+                  <li class="media">
+                    <a onclick="refresh_links()" href="#">أدرس السوق</a>
+                  </li>
+                </ul> -->
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" id="headingTwo">
+              <h5 class="mb-0 text-center">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  الدرس الثاني
+                  <spin class="circle-icon mr-4">2</spin>
+                </button>
+              </h5>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+
+              <div class="container">
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  <a onclick="refresh_links()" href="#">ابحث عن الأمور التي تثير الفضول</a>
+                </label>
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  المتطلبات
+                </label>
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  الحلول
+                </label>
+              </div>
+
+              <!-- <ul class="list-unstyled ui-steps">
+                  <li class="media">
+
+                    <a onclick="refresh_links()" href="#">ابحث عن الأمور التي تثير الفضول</a>
+                  </li>
+                  <li class="media">
+                    <a onclick="refresh_links()" href="#" title="المتطلبات">المتطلبات</a>
+                  </li>
+
+                </ul> -->
+
+
+
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" id="headingThree">
+              <h5 class="mb-0 text-center">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  الدرس الثالث
+                  <spin class="circle-icon mr-4">3</spin>
+                </button>
+              </h5>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+              <div class="container">
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  <a onclick="refresh_links()" href="#">ابحث عن الأمور التي تثير الفضول</a>
+                </label>
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  المتطلبات
+                </label>
+                <label class="row">
+                  <input onclick="refresh_links()" type="radio" class="option-input radio" name="example" />
+                  الحلول
+                </label>
+              </div>
+
+              <!-- <ul class="list-unstyled ui-steps">
+                <li class="media">
+
+                  <a onclick="refresh_links()" href="#">المقدمة</a>
+                </li>
+                <li class="media">
+                  <a onclick="refresh_links()" href="#" title="المتطلبات">المتطلبات</a>
+                </li>
+
+              </ul> -->
+
+            </div>
+          </div>
+        </div>
+
+        <div class="card widget">
+          <!-- <div class="card-header"> -->
+          <h3 class="text-right">
+            <i class="fa fa-star"></i>
+            تقييم الكورس
+          </h3>
+
+          <!-- </div> -->
+          <div class="card-body">
+
+            <div class="rating-result">5</div>
+            <div class="rating-start rating-large">
+
+              <i class="fa fa-star" style="color:#F7CA18;"></i>
+              <i class="fa fa-star" style="color:#F7CA18;"></i>
+              <i class="fa fa-star" style="color:#F7CA18;"></i>
+              <i class="fa fa-star" style="color:#F7CA18;"></i>
+              <i class="fa fa-star" style="color:#F7CA18;"></i>
+            </div>
+            <div class="rating-user">
+              <i class="fa fa-user"></i> 6
+            </div>
+          </div>
+
+          <div class="view-all-rating">
+            <a href="#/reviews/350">
+              عرض تقييمات الطلاب
+            </a>
+          </div>
+        </div>
+        <div class="widget share-course">
+          <h3 class="text-right">
+            <i class="fa fa-share-alt"></i>
+            شارك الكورس
+          </h3>
+          <div class="share-buttons">
+            <input class="form-control" onclick="selectInput(this)" value="http://127.0.0.1:8000/courses" readonly="">
+          </div>
+
+          <ul class="nav list-unstyled share-buttons">
+            <li>
+              <a class="sharing-button google-plus" href="#" title="google plus" target="_blank">
+                <i class="fa fa-google-plus"></i>
+              </a>
+            </li>
+
+            <li>
+              <a class="sharing-button email" href="#" title="email" target="_self">
+                <i class="fa fa-envelope"></i>
+              </a>
+            </li>
+            <li>
+              <a class="sharing-button twitter" href="#/" title="twitter" target="_blank">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a class="sharing-button google-plus" href="#" title="google plus" target="_blank">
+                <i class="fa fa-google-plus"></i>
+              </a>
+            </li>
+            <li>
+              <a class="sharing-button email" href="#" title="email" target="_self">
+                <i class="fa fa-envelope"></i>
+              </a>
+            </li>
+            <li>
+              <a class="sharing-button whatsapp" href="#" title="whatsapp" target="_blank">
+                <i class="fa fa-whatsapp"></i>
+              </a>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+
+      <div class="main">
+        <div id="wrapper">
+          <div id="content">
+            <div id="links_container">
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- </div> -->
+
+
+    <div id="footer" class="footer">
+      <p>Footer</p>
+    </div>
+
+
+
+  </div>
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      // $('.navbar-collapse a').click(function() {
+      //   $(".navbar-collapse").collapse('hide');
+      // });
+      // $('li .classloader').click(function() {
+
+      //   $('#content').load('content.php');
+      // });
+
+
+
+
+      window.refresh_links = function() {
+
+        $.get('/getLinks', function(data) {
+          document.getElementById('links_container').innerHTML = data;
+        })
+      }
+
+    })
+  </script>
+  <script src="assets/js/jquery-3.2.1.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <!-- Plugins JS -->
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <!-- Custom JS -->
+  <script src="assets/js/script.js"></script>
+</body>
+
+</html>
